327ea089b45036b6a1db1ca5ca3698665cc9ef0b
Gabriel Melo <bielrj170@gmail.com>
2024-09-07 23:16:33 -0300

    feat: add a home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,8 @@
+import CharacterList from "../../components/characterList";
+import Header from "../../components/header";
+
+const Home = () => {
+	return <CharacterList />;
+};
+
+export default Home;
