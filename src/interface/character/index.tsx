b0fea07265acbbb120365cbe5565ccd5437d250f
Gabriel Melo <bielrj170@gmail.com>
2024-09-07 23:16:24 -0300

    feat: add a character interface

---
+++ src/interface/character/index.tsx
@@ -0,0 +1,14 @@
+interface CharacterInterface {
+	id: number;
+	imagem: string;
+	alt: string;
+	nome: string;
+	nickname: string;
+	dragao?: string; // opcional
+	age: number;
+	enemies: string;
+	house: string;
+	hoverImage: string;
+}
+
+export default CharacterInterface;
