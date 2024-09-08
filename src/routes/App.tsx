7eba1d04332b7f78a2f6a9a243a355c380bb87c5
Gabriel Melo <bielrj170@gmail.com>
2024-09-07 23:15:08 -0300

    feat: finish routes with provider context, and default routes

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/home";
+import CharacterProvider from "../context";
+import FixedLayout from "../components/fixedLayout";
+import CharacterDetail from "../components/characterDetail";
+
 function AppRoutes() {
-	return <></>;
+	return (
+		<>
+			<BrowserRouter>
+				<CharacterProvider>
+					<Routes>
+						<Route path="/" element={<FixedLayout />}>
+							<Route path="/" element={<Home />} />
+							<Route
+								path="/characterDetail/:idParams"
+								element={<CharacterDetail />}
+							/>
+						</Route>
+					</Routes>
+				</CharacterProvider>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default AppRoutes;
