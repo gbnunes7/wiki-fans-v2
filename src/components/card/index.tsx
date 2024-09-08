f3d218c3130ee50849562b1bb803452a92f6b34e
Gabriel Melo <bielrj170@gmail.com>
2024-09-07 23:15:34 -0300

    feat: finish character card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,18 @@
+const Card = ({
+	src,
+	alt,
+	nome,
+}: {
+	src: string;
+	alt: string;
+	nome: string;
+}) => {
+	return (
+		<div className="bg-red-400 min-h-52 flex flex-col items-center p-2 rounded shadow-md shadow-black hover:cursor-pointer hover:scale-110 duration-100 ">
+			<img src={src} className="w-28 h-44 rounded m-2 object-cover" alt={alt} />
+			<p>{nome}</p>
+		</div>
+	);
+};
+
+export default Card;
